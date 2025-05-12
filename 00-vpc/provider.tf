72aa91ee7ef585d05e35d472249f468f57b07e38
svrkrishna-ch <ramakrishna.chilla074@gmail.com>
2025-05-12 06:14:59 +0530

    terraform

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "82s-tf-remote-state-dev"
+    bucket = "svrk-tf-state-dev"
     key    = "expense-dev-vpc" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "svrk-tf-state-dev"
